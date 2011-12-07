503cb270616c91aaed906f398bb361f16f5524ae
Ben Dornis <ben@buildstarted.com>
2011-12-07 13:57:36 -0800

    initial commit of signalR.eventStream.js

---
+++ src/SignalR.EventStream/signalR.eventStream.js
@@ -0,0 +1,30 @@
+﻿function EventStream() {
+    this.initialized = false;
+    this.connect = function () {
+        if (!this.initialized) {
+            var stream = $.connection.eventStream;
+            var parent = this;
+            stream.receiveEvent = function (event) {
+                var result = $.parseJSON(event);
+                var template = $("#eventStream-template-" + result.Type);
+                if (template && template.tmpl) {
+                    parent.eventReceived(result, template.tmpl(result.Type == "event" ? result : result.Event));
+                } else {
+                    parent.eventReceived(result);
+                }
+            };
+
+            $.connection.hub.start({}, function () {
+                stream.authorize();
+            });
+
+            this.initialized = true;
+        }
+
+        return this;
+    };
+
+    this.eventReceived = function (data, template) {
+        $("body").append(template);
+    };
+};
